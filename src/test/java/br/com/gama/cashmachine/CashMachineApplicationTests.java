ef78322ac4427f7e981cf25516c7079911f5aefb
Daniel Versiane <daniel.versiane@gmail.com>
2021-11-01 19:07:21 -0300

    :tada: first commit

---
+++ src/test/java/br/com/gama/cashmachine/CashMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gama.cashmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
