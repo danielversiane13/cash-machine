2b6bdd32d95ad6562be8fbf72681955bcaacf49f
Daniel Versiane <daniel.versiane@gmail.com>
2021-11-02 11:25:37 -0300

    feat: add logic to withdraw money bills from machine

--- src/main/java/br/com/gama/cashmachine/services/CurrentAccountService.java
+++ src/main/java/br/com/gama/cashmachine/services/CurrentAccountService.java
@@ -2,6 +2,7 @@ package br.com.gama.cashmachine.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.gama.cashmachine.dto.CurrentAccountWithdrawDto;
 import br.com.gama.cashmachine.entities.Machine;
+import br.com.gama.cashmachine.entities.MachineMoneyBills;
 import br.com.gama.cashmachine.entities.MoneyBills;
 import br.com.gama.cashmachine.exceptions.BadRequestException;
 import br.com.gama.cashmachine.exceptions.ExceptionHandler;
@@ -16,6 +18,7 @@ import br.com.gama.cashmachine.exceptions.NotAcceptableException;
 import br.com.gama.cashmachine.exceptions.NotFoundException;
 import br.com.gama.cashmachine.factories.CurrentAccountWithdrawFactory;
 import br.com.gama.cashmachine.forms.CurrentAccountWithdrawForm;
+import br.com.gama.cashmachine.repositories.MachineMoneyBillsRepository;
 import br.com.gama.cashmachine.repositories.MachineRepository;
 import br.com.gama.cashmachine.repositories.MoneyBillsRepository;
 
@@ -27,6 +30,9 @@ public class CurrentAccountService {
 
 	@Autowired
 	private MoneyBillsRepository moneyBillsRepository;
+
+	@Autowired
+	private MachineMoneyBillsRepository machineMoneyRepository;
 
 	public List<CurrentAccountWithdrawDto> withdraw(UUID machineId, CurrentAccountWithdrawForm form)
 			throws ExceptionHandler {
@@ -49,19 +55,35 @@ public class CurrentAccountService {
 		int _value = form.value;
 		List<MoneyBills> moneyBills = moneyBillsRepository.findByActiveTrueOrderByValueDesc();
 		List<CurrentAccountWithdrawDto> withdrawList = new ArrayList<CurrentAccountWithdrawDto>();
+		Set<MachineMoneyBills> machineMoney = machine.getMoneyBills();
+		List<MachineMoneyBills> listToSave = new ArrayList<MachineMoneyBills>();
 
 		for (int i = 0; i < moneyBills.size(); i++) {
 			MoneyBills money = moneyBills.get(i);
 			int quantity = _value / money.getValue();
-			_value = _value - (money.getValue() * quantity);
-
-			// TODO: Add validation quantity of money bills
 
 			// Case needs values only > 0
 //			if (quantity == 0) {
 //				continue;
 //			}
 
+			if (quantity > 0) {
+				MachineMoneyBills machineMoneyFind = machineMoney.stream()
+						.filter(mm -> mm.getMoneyBills().getId() == money.getId()).findFirst().get();
+
+				if (machineMoneyFind.getQuantity() > 0) {
+					if (machineMoneyFind.getQuantity() < quantity) {
+						quantity = machineMoneyFind.getQuantity();
+					}
+
+					machineMoneyFind.setQuantity(machineMoneyFind.getQuantity() - quantity);
+					listToSave.add(machineMoneyFind);
+				} else {
+					quantity = 0;
+				}
+			}
+
+			_value = _value - (money.getValue() * quantity);
 			withdrawList.add(CurrentAccountWithdrawFactory.Create(money.getLabel(), quantity));
 
 			// Case needs values only > 0
@@ -70,8 +92,11 @@ public class CurrentAccountService {
 //			}
 		}
 
-		// TODO: Add validation if _value > 0, throw error 406
+		if (_value > 0) {
+			throw new NotAcceptableException("Caixa temporariamente indisponível");
+		}
 
+		machineMoneyRepository.saveAll(listToSave);
 		machine.setBalance(machine.getBalance() - form.value);
 		machineRepository.save(machine);
 
