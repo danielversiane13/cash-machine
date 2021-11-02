ef00d9fc29fa71a0270e117eb2b6465aad6c57fa
Daniel Versiane <daniel.versiane@gmail.com>
2021-11-02 16:10:22 -0300

    feat: add method to initialize project with machines and moneybills

--- src/main/java/br/com/gama/cashmachine/CashMachineApplication.java
+++ src/main/java/br/com/gama/cashmachine/CashMachineApplication.java
@@ -1,13 +1,53 @@
 package br.com.gama.cashmachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.gama.cashmachine.entities.Machine;
+import br.com.gama.cashmachine.entities.MachineMoneyBills;
+import br.com.gama.cashmachine.entities.MoneyBills;
+import br.com.gama.cashmachine.repositories.MachineMoneyBillsRepository;
+import br.com.gama.cashmachine.repositories.MachineRepository;
+import br.com.gama.cashmachine.repositories.MoneyBillsRepository;
+
 @SpringBootApplication
-public class CashMachineApplication {
+public class CashMachineApplication implements CommandLineRunner {
+
+	@Autowired
+	private MachineRepository machineRepository;
+
+	@Autowired
+	private MoneyBillsRepository moneyBillsRepository;
+
+	@Autowired
+	private MachineMoneyBillsRepository machineMoneyBillsRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CashMachineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		int amountMoneyBills = 5;
+
+		List<Machine> machines = new ArrayList<Machine>(List.of(new Machine("Machine 01"), new Machine("Machine 02")));
+		machineRepository.saveAll(machines);
+
+		List<MoneyBills> moneyBills = moneyBillsRepository.findAll();
+
+		machines.stream().forEach(machine -> {
+			moneyBills.stream().forEach(money -> {
+				machineMoneyBillsRepository.save(new MachineMoneyBills(machine, money, amountMoneyBills));
+				int newBalance = machine.getBalance() + money.getValue() * amountMoneyBills;
+				machine.setBalance(newBalance);
+			});
+			machineRepository.save(machine);
+		});
 	}
 
 }
